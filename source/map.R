ed8488a142f68bd192c41c6db9c930859f1745a1
tomjubin <97139089+tomjubin@users.noreply.github.com>
2022-02-25 13:30:45 -0800

    committed the map

--- source/map.R
+++ source/map.R
@@ -1,9 +1,8 @@
-#A3 - Tom Jubin - analysis.R file
+#A3 - Tom Jubin - map
 
 library(tidyverse)
 library(dplyr)
 library(maps)
-library(mapproj)
 library(patchwork)
 library(plotly)
 library(usmap)
@@ -12,17 +11,22 @@ library(ggplot2)
 incarcerations_table <- read_incarcerations_trends()
 
 cleandata <- incarcerations_table %>%
-  filter(year == 2018) %>%
+  filter(year == 2016) %>%
   mutate(Year = year, state = state, total_jail_from_ice) %>%
   group_by(state) %>%
   summarize(Total_Jail_Population_From_Ice = sum(total_jail_from_ice)) %>%
   select(state, Total_Jail_Population_From_Ice)
 
-
-
-Mapp <- plot_usmap(data = cleandata, values = "Total_Jail_Population_From_Ice", color = "red") + 
-  labs(title = "US",
-       subtitle = "This is a blank map of the counties of the United States.") + 
+Mappp <- plot_usmap(data = cleandata, values = "Total_Jail_Population_From_Ice", color = "black") +
+  scale_fill_continuous(low = "white", high = "skyblue", name = "Total Jail Population from Ice (2016)", label = scales::comma) +
+  labs(title = "USA Map") + 
   theme_minimal()
 
-ggplotly(Mapp)
+ggplotly(Mappp)
+
+#Significant Numbers
+
+max_jail_from_ice <- cleandata %>%
+  filter(Total_Jail_Population_From_Ice == max(Total_Jail_Population_From_Ice, na.rm = TRUE)) %>%
+  pull(Total_Jail_Population_From_Ice)
+
