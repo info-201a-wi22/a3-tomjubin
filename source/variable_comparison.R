2e556ada2b577d5c87dedc3803e913622445aea4
tomjubin <97139089+tomjubin@users.noreply.github.com>
2022-02-25 14:16:13 -0800

    committed the variable comparison

--- source/variable_comparison.R
+++ source/variable_comparison.R
@@ -1,19 +1,31 @@
-#A3 - Tom Jubin - analysis.R file
+#A3 - Tom Jubin - variable comparison
 
 library(ggplot2)
 library(dplyr)
-library(reshape2)
 library(plotly)
 library(tidyr)
 
 incarcerations_table <- read_incarcerations_trends()
 
 cleandata <- incarcerations_table %>%
-  mutate(Year = year, Female_Jail_Population = female_jail_pop, Male_Jail_Population = male_jail_pop) %>%
-  select(Year, Female_Jail_Population, Male_Jail_Population)
+  mutate(Year = year, Female_Jail_Population_Age_15_64 = female_pop_15to64, Male_Jail_Population_Age_15_64 = male_pop_15to64) %>%
+  select(Year, Female_Jail_Population_Age_15_64, Male_Jail_Population_Age_15_64)
 
-boyvgirl <- ggplot(data=cleandata, aes(x=Year))+
-  geom_line(aes(y = Female_Jail_Population), color = "black") +
-  geom_line(aes(y = Male_Jail_Population), color = "yellow") 
+comparison <- ggplot(data=cleandata, aes(x=Year))+
+  geom_line(aes(y = Female_Jail_Population_Age_15_64), color = "green") +
+  geom_line(aes(y = Male_Jail_Population_Age_15_64), color = "purple") +
+  labs(title = "Female vs Male Age 15-64 Jail Population", x = "Year", y = "Population")
 
-ggplotly(boyvgirl)
+ggplotly(comparison)
+
+#Signifcant Values
+
+max_female_15_64 <- cleandata %>%
+  filter(Female_Jail_Population_Age_15_64 == max(Female_Jail_Population_Age_15_64, na.rm=TRUE)) %>%
+  pull(Female_Jail_Population_Age_15_64)
+  #2015
+
+max_male_15_64 <- cleandata %>%
+  filter(Male_Jail_Population_Age_15_64 == max(Male_Jail_Population_Age_15_64, na.rm=TRUE)) %>%
+  pull(Male_Jail_Population_Age_15_64)
+  #2016
