73e0ea4870b9f53015e5659b3149a42eb631639b
tomjubin <97139089+tomjubin@users.noreply.github.com>
2022-02-25 13:29:58 -0800

    committed graph over time

--- source/graph_over_time.R
+++ source/graph_over_time.R
@@ -1,4 +1,4 @@
-#A3 - Tom Jubin - analysis.R file
+#A3 - Tom Jubin - graph over time
 
 library(ggplot2)
 library(dplyr)
@@ -11,16 +11,28 @@ cleandata <- incarcerations_table %>%
   mutate(Year = year, Total_Black_Jail_Population = black_jail_pop, Total_White_Jail_Population = white_jail_pop, Total_Jail_Population = total_jail_pop) %>%
   select(Year, Total_Black_Jail_Population, Total_White_Jail_Population, Total_Jail_Population)
 
-line_graph <- ggplot(data=cleandata, aes(x=Year))+
-  geom_line(aes(y = Total_Jail_Population), color = "green") +
+time_graph <- ggplot(data=cleandata, aes(x=Year))+
+  geom_line(aes(y = Total_Jail_Population), color = "yellow") +
   geom_line(aes(y = Total_Black_Jail_Population), color = "black") +
-  geom_line(aes(y = Total_White_Jail_Population), color = "blue", linetype = "twodash") 
-  #labs(title = "Black v White v Total ", 
-      # x = "Years", 
-       #y = "Blah",
-       #color = "Blah",
-       #subtitle = "Blah",
-       #caption = "Blah")+
-  #theme_minimal()
+  geom_line(aes(y = Total_White_Jail_Population), color = "white") + 
+  labs(title = "Total vs Black vs White Jail Population ", 
+       x = "Year", 
+       y = "Population")
 
-ggplotly(line_graph)
+ggplotly(time_graph)
+
+#Signficant Values
+
+max_black_jail_pop <- cleandata %>%
+  filter(Total_Black_Jail_Population == max(Total_Black_Jail_Population, na.rm=TRUE)) %>%
+  pull(Total_Black_Jail_Population)
+ 
+max_white_jail_pop_during_max_black_jail_pop <- cleandata %>%
+  filter(Year == 1993) %>%
+  filter(Total_White_Jail_Population == max(Total_White_Jail_Population, na.rm=TRUE)) %>%
+  pull(Total_White_Jail_Population) 
+
+total_jail_pop_during_max_black_jail_pop <- cleandata %>%
+  filter(Year == 1993) %>%
+  filter(Total_Jail_Population == max(Total_Jail_Population, na.rm = TRUE)) %>%
+  pull(Total_Jail_Population)
